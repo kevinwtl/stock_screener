7262d0cb1fdc4bf5a8a374b6141870568638a306
kevinwtl <tinglam@TingLams-MacBook-Pro.local>
2020-07-18 18:57:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 app.layout = html.Div([
 
