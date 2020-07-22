05542997fd1665f712c6509b4c4f9e0b90696218
kevinwtl <tinglam@TingLams-MacBook-Pro.local>
2020-07-22 18:58:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 from bs4 import BeautifulSoup
-#from value_investing.fin_scraping import *
 from fin_scraping import all_pages_scraped
 import requests
 import pandas as pd
